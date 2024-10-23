36bc48e5f0a6474f0397e11a049dea984e22ed27
Amir <amir@infosec>
2024-10-23 13:27:03 -0400

    join files by extention

--- filescan.py
+++ filescan.py
@@ -25,10 +25,25 @@ print(extensions)
 
 
 temp_dir = subprocess.check_output(['mktemp', '-d']).decode('utf-8').strip()
-for i in users:
-    for j in extensions[f"/home/{i}/.vscode/extensions"]:
-        os.makedirs(os.path.join(temp_dir, i, j), exist_ok=True)
-        ext_files = subprocess.run(['find', '/home/user/.vscode/extensions/j', '-type', 'f'])for i in extensions.keys():
+for user in users:
+    for ext_name in extensions[f"/home/{user}/.vscode/extensions"]:
+        os.makedirs(os.path.join(temp_dir, user, ext_name), exist_ok=True)
+        ext_files = subprocess.run(['find', f'/home/{user}/.vscode/extensions/{ext_name}', '-type', 'f'], capture_output=True, text=True).stdout.splitlines()
+        for  file in ext_files:
+            rash = file.split('.')[-1:][0]
+            print(rash)
+            
+            try:
+                if len(rash) < 5:
+                    with open(os.path.join(temp_dir, user, ext_name, "file."+rash), 'a') as f:
+                        with open(file, 'rb') as e:
+                            f.write(str(e.read())[2:-1]+"\n")
+                else:
+                    raise  Exception("File name too long")
+            except:
+                with open(os.path.join(temp_dir, user, ext_name, "file.txt"), 'a') as f:
+                    with open(file, 'rb') as e:
+                        f.write(str(e.read())[2:-1]+"\n")
 
 
 print(temp_dir)
